2554652e8ae5f74ef97ac4e5cb5e59acf09351ce
steve111MV <unlivregratuit@gmail.com>
2017-05-06 12:29:02 +0100

    fix: SyncAdapter debugged and working perfectly
    There is a Bug with the present code,
    Volley can't handle Redirection, error 301 thrown

--- app/src/main/java/cg/stevendende/deliciousrecipes/MainActivity.java
+++ app/src/main/java/cg/stevendende/deliciousrecipes/MainActivity.java
@@ -1,17 +1,35 @@
 package cg.stevendende.deliciousrecipes;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import cg.stevendende.deliciousrecipes.sync.RecipesSyncAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    @SuppressWarnings("WeakerAccess")
+    @BindView(R.id.swipeRefresh)
+    SwipeRefreshLayout swipeRefresh;
+
+
+ @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        RecipesSyncAdapter.initializeSyncAdapter(this);
+
+     swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+         @Override
+         public void onRefresh() {
+             RecipesSyncAdapter.syncImmediately(MainActivity.this);
+         }
+     });
+
     }
 }
