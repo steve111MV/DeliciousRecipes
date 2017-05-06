827fd24f1f0b1484b33c3c8ace368105d3f1eb64
steve111MV <unlivregratuit@gmail.com>
2017-05-06 08:18:13 +0100

    feat: Add database stucture for the API contents

---
+++ app/src/main/java/cg/stevendende/deliciousrecipes/Constants.java
@@ -0,0 +1,63 @@
+package cg.stevendende.deliciousrecipes;
+
+import cg.stevendende.deliciousrecipes.data.RecipesContract;
+
+/**
+ * Created by STEVEN on 06/05/2017.
+ */
+
+public class Constants {
+    // Create a table to hold Recipes
+    public static final String SQL_CREATE_RECIPES_TABLE = "CREATE TABLE " + RecipesContract.RecipeEntry.TABLE_NAME + " (" +
+
+            // the ID of the recipe entry as returned by the API
+            RecipesContract.RecipeEntry._ID + " INTEGER," +
+
+            RecipesContract.RecipeEntry.COLUMN_NAME + " TEXT NOT NULL, " +
+            RecipesContract.RecipeEntry.COLUMN_SERVINGS + " INTEGER DEFAULT 0," +
+            RecipesContract.RecipeEntry.COLUMN_IMAGE + " TEXT," +
+
+            // To ensure the application doesn't duplicate recipes in database,
+            // we create a UNIQUE constraint
+            " UNIQUE (" + RecipesContract.RecipeEntry._ID + ") );";
+
+
+    // Create a table to hold Recipe Ingredients
+    public static final String SQL_CREATE_RECIPE_INGREDIENTS_TABLE = "CREATE TABLE " + RecipesContract.RecipeEntry.TABLE_NAME + " (" +
+
+            // the ID of the recipe entry as returned by the API
+            RecipesContract.IngredientEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+
+            RecipesContract.IngredientEntry.COLUMN_QUANTITY + " DOUBLE DEFAULT 0, " +
+            RecipesContract.IngredientEntry.COLUMN_MEASURE + " TEXT," +
+            RecipesContract.IngredientEntry.COLUMN_INGREDIENT+ " TEXT NOT NULL," +
+
+            // Set up the "RecipeStepEntry.COLUMN_RECIPE_ID" column
+            // as a foreign key to recipes table.
+            " FOREIGN KEY (" + RecipesContract.RecipeStepEntry.COLUMN_RECIPE_ID + ") REFERENCES " +
+            RecipesContract.RecipeEntry.TABLE_NAME + " (" + RecipesContract.RecipeEntry._ID + ") );";
+
+
+    // Create a table to hold Recipes
+    public static final String SQL_CREATE_RECIPE_STEPS_TABLE = "CREATE TABLE " + RecipesContract.RecipeStepEntry.TABLE_NAME + " (" +
+
+            // the ID of the recipe entry as returned by the API
+            RecipesContract.RecipeStepEntry._ID + " INTEGER," +
+
+            RecipesContract.RecipeStepEntry.COLUMN_SHORT_DESCRIPTION + " TEXT, " +
+            RecipesContract.RecipeStepEntry.COLUMN_DESCRIPTION + " TEXT, " +
+            RecipesContract.RecipeStepEntry.COLUMN_VIDEO_URL + " TEXT, " +
+            RecipesContract.RecipeStepEntry.COLUMN_IMAGE_URL + " TEXT, " +
+
+            RecipesContract.RecipeStepEntry.COLUMN_RECIPE_ID + " INTEGER," +
+
+            // Set up the "IngredientEntry.COLUMN_RECIPE_ID" column
+            // as a foreign key to recipes table.
+            " FOREIGN KEY (" + RecipesContract.IngredientEntry.COLUMN_RECIPE_ID + ") REFERENCES " +
+            RecipesContract.RecipeEntry.TABLE_NAME + " (" + RecipesContract.RecipeEntry._ID + "), " +
+
+            // To ensure the application doesn't duplicate recipes,
+            // we create a UNIQUE constraint
+            " UNIQUE (" + RecipesContract.RecipeStepEntry._ID + ") );";
+
+}
---
+++ app/src/main/java/cg/stevendende/deliciousrecipes/data/RecipesContract.java
@@ -0,0 +1,122 @@
+package cg.stevendende.deliciousrecipes.data;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by STEVEN on 06/05/2017.
+ */
+
+public class RecipesContract {
+
+    public static final String DATABASE_NAME = "delrecipes.db";
+
+    // The "Content authority" is a name for the entire content provider, similar to the
+    // relationship between a domain name and its website.  A convenient string to use for the
+    // content authority is the package name for the app, which is guaranteed to be unique on the
+    // device.
+    public static final String CONTENT_AUTHORITY = "cg.stevendende.deliciousrecipes";
+
+    // The CONTENT_AUTHORITY is used to create the base of all URI's which apps will use to contact
+    // the content provider.
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // Possible paths
+    public static final String PATH_RECIPES = "recipes";
+    public static final String PATH_RECIPE_INGREDIENTS = "ingredients";
+    public static final String PATH_RECIPE_STEPS = "steps";
+
+
+    /* Inner class that defines the table contents of Movie table */
+    public static final class RecipeEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_RECIPES).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_RECIPES;
+
+        // Table name
+        public static final String TABLE_NAME = "recipes";
+
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_SERVINGS = "servings";
+        public static final String COLUMN_IMAGE = "image";
+
+        public static Uri buildRecipeUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static final String[] COLUMNS_RECIPES = new String[]{
+                RecipesContract.RecipeEntry.TABLE_NAME+"."+RecipesContract.RecipeEntry._ID,
+                RecipesContract.RecipeEntry.TABLE_NAME+"."+ RecipeEntry.COLUMN_NAME,
+                RecipesContract.RecipeEntry.TABLE_NAME+"."+RecipesContract.RecipeEntry.COLUMN_SERVINGS,
+                RecipesContract.RecipeEntry.TABLE_NAME+"."+RecipesContract.RecipeEntry.COLUMN_IMAGE
+        };
+    }
+
+    /* Inner class that defines the table contents of Ingredients table */
+    public static final class IngredientEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_RECIPE_INGREDIENTS).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_RECIPE_INGREDIENTS;
+
+        // Table name
+        public static final String TABLE_NAME = "ingredients";
+
+        public static final String COLUMN_QUANTITY = "quantity";
+        public static final String COLUMN_MEASURE = "measure";
+        public static final String COLUMN_INGREDIENT = "ingredient";
+
+        public static final String COLUMN_RECIPE_ID = "recipe_id";
+
+        public static Uri buildIngedientUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static final String[] COLUMNS_INGREDIENTS = new String[]{
+                RecipesContract.IngredientEntry.TABLE_NAME+"."+RecipesContract.IngredientEntry._ID,
+                RecipesContract.IngredientEntry.TABLE_NAME+"."+ IngredientEntry.COLUMN_QUANTITY,
+                RecipesContract.IngredientEntry.TABLE_NAME+"."+ IngredientEntry.COLUMN_MEASURE,
+                RecipesContract.IngredientEntry.TABLE_NAME+"."+ IngredientEntry.COLUMN_INGREDIENT
+        };
+    }
+
+    /* Inner class that defines the table contents of Recipe-Steps table */
+    public static final class RecipeStepEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_RECIPE_STEPS).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_RECIPE_STEPS;
+
+        // Table name
+        public static final String TABLE_NAME = "steps";
+
+        public static final String COLUMN_SHORT_DESCRIPTION = "short_desc";
+        public static final String COLUMN_DESCRIPTION = "desc";
+        public static final String COLUMN_VIDEO_URL = "video_url";
+        public static final String COLUMN_IMAGE_URL = "thumbnail_url";
+
+        public static final String COLUMN_RECIPE_ID = "recipe_id";
+
+        public static Uri buildStepUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static final String[] COLUMNS_STEPS = new String[]{
+                RecipesContract.RecipeStepEntry.TABLE_NAME+"."+RecipesContract.RecipeStepEntry._ID,
+                RecipesContract.RecipeStepEntry.TABLE_NAME+"."+ RecipeStepEntry.COLUMN_SHORT_DESCRIPTION,
+                RecipesContract.RecipeStepEntry.TABLE_NAME+"."+ RecipeStepEntry.COLUMN_DESCRIPTION,
+                RecipesContract.RecipeStepEntry.TABLE_NAME+"."+ RecipeStepEntry.COLUMN_VIDEO_URL,
+                RecipesContract.RecipeStepEntry.TABLE_NAME+"."+ RecipeStepEntry.COLUMN_IMAGE_URL
+        };
+    }
+
+    }
