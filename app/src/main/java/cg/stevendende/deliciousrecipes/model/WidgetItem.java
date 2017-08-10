7ef132c1235f06f2ac925d04de6be12d0c58b25e
steve111MV <unlivregratuit@gmail.com>
2017-08-10 18:01:50 +0100

    refactor: add WidgetItem object to use in RemoteAdapter

---
+++ app/src/main/java/cg/stevendende/deliciousrecipes/model/WidgetItem.java
@@ -0,0 +1,14 @@
+package cg.stevendende.deliciousrecipes.model;
+
+/**
+ * Created by STEVEN on 10/08/2017.
+ */
+
+public class WidgetItem {
+
+    public String text;
+
+    public WidgetItem(String arg) {
+        this.text = arg;
+    }
+}
